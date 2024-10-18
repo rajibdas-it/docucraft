150fa2f45a0382d194ed73f7381daa5735935095
rajibdas-it <rajibrad@gmail.com>
2024-10-19 02:13:21 +0600

    complete

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,9 @@
 import Header from "@/components/Header";
 import "./globals.css";
 import { getDoucments } from "@/lib/doc";
+import { Suspense } from "react";
+import Loading from "@/components/Loading";
+
 
 
 export const metadata = {
@@ -16,25 +19,28 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
         <div className="h-full lg:ml-72 xl:ml-80">
-          <Header docs={allDocuments} />
-          <div className="relative px-4 pt-14 sm:px-6 lg:px-8">
-            <main className="flex-auto py-16">
-              <div className="absolute inset-0 -z-10 mx-0 max-w-none overflow-hidden">
-                <div
-                  className="absolute left-1/2 top-0 ml-[-38rem] h-[25rem] w-[81.25rem] dark:[mask-image:linear-gradient(white,transparent)]"
-                >
+
+          <Suspense fallback={<Loading />}>
+            <Header docs={allDocuments} />
+
+            <div className="relative px-4 pt-14 sm:px-6 lg:px-8">
+              <main className="flex-auto py-16">
+                <div className="absolute inset-0 -z-10 mx-0 max-w-none overflow-hidden">
                   <div
-                    className="absolute inset-0 bg-gradient-to-r from-[#36b49f] to-[#DBFF75] opacity-40 [mask-image:radial-gradient(farthest-side_at_top,white,transparent)] dark:from-[#36b49f]/30 dark:to-[#DBFF75]/30 dark:opacity-100"
-                  ></div>
+                    className="absolute left-1/2 top-0 ml-[-38rem] h-[25rem] w-[81.25rem] dark:[mask-image:linear-gradient(white,transparent)]"
+                  >
+                    <div
+                      className="absolute inset-0 bg-gradient-to-r from-[#36b49f] to-[#DBFF75] opacity-40 [mask-image:radial-gradient(farthest-side_at_top,white,transparent)] dark:from-[#36b49f]/30 dark:to-[#DBFF75]/30 dark:opacity-100"
+                    ></div>
+                  </div>
                 </div>
-              </div>
 
-              {children}
+                {children}
 
-            </main>
-          </div>
+              </main>
+            </div>
+          </Suspense>
         </div>
-
       </body>
     </html>
   );
